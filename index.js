b05e86a7195b8a661f2ed69c69695c35c57b3abe
Kevin Martensson <kevinmartensson@gmail.com>
2015-01-28 18:31:47 +0100

    Tweaks

--- index.js
+++ index.js
@@ -2,13 +2,6 @@
 
 var isPathGlobal = require('is-path-global');
 
-/**
- * Get all global paths from an array
- *
- * @param {Array} arr
- * @api public
- */
-
 module.exports = function (arr) {
 	return arr.filter(function (path) {
 		return isPathGlobal(path);
